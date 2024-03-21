6f7778cd0c6c2bd4e901708afdf219a096aefaa6
leehyebin <970905hyebin@gmail.com>
2024-03-22 05:08:12 +0900

    Feat:jwt 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,26 @@ import { UserService } from './services';
 import { UserRepository } from './repositories';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<JwtModuleOptions> => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: {
+          expiresIn: configService.get<string>('ACCESS_TOKEN_EXPIRY'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, UserService, UserRepository],
+  providers: [AuthService, UserService, ConfigService, UserRepository],
 })
 export class AuthModule {}
