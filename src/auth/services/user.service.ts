8360ee17d726ac659d7da90a8cd3fd7a936d5dae
leehyebin <970905hyebin@gmail.com>
2024-03-22 05:10:31 +0900

    Feat: user validate 기능 추가

--- src/auth/services/user.service.ts
+++ src/auth/services/user.service.ts
@@ -1,8 +1,13 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { createUserDto } from '../dto/create-user.dto';
 import { SignupResDto } from '../dto/signup-res.dto';
 import { UserRepository } from '../repositories';
 import * as bcrypt from 'bcrypt';
+import { User } from '../entities';
 
 @Injectable()
 export class UserService {
@@ -11,17 +16,21 @@ export class UserService {
     const { email, password } = createUserDto;
 
     const isExist = await this.userRepo.findUserByEmail(email);
-
     if (isExist) throw new ConflictException('이미 존재하는 이메일 입니다.');
 
     const hashedPassword = await this.generateHashedPassword(password);
-
     return this.userRepo.createUser(createUserDto, hashedPassword);
   }
 
   async generateHashedPassword(password: string) {
     const salt = await bcrypt.genSalt();
-
     return bcrypt.hash(password, salt);
+  }
+
+  async validateUser(id: string): Promise<User> {
+    const user = this.userRepo.findOneBy({ id });
+    if (!user) throw new NotFoundException('유저가 존재하지 않습니다.');
+
+    return user;
   }
 }
