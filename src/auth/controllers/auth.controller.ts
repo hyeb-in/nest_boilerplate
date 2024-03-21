55c7cb41fa3b7218324e46923197763d65f52897
leehyebin <970905hyebin@gmail.com>
2024-03-22 05:12:19 +0900

    Feat:로그인 컨트롤러 추가

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { SignupResDto } from '../dto/signup-res.dto';
 import { createUserDto } from '../dto/create-user.dto';
-import { UserService } from '../services';
+import { AuthService, UserService } from '../services';
+import { loginReqDto } from '../dto/login-req.dto';
+import { LoginResDto } from '../dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
   @Post('/signup')
   async signup(@Body() createUserDto: createUserDto): Promise<SignupResDto> {
     const user = await this.userService.createUser(createUserDto);
@@ -14,5 +20,22 @@ export class AuthController {
       name: user.name,
       email: user.email,
     };
+  }
+
+  @Post('/login')
+  async login(
+    @Body() loginReqDto: loginReqDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<LoginResDto> {
+    const { accessToken, refreshToken, user } = await this.authService.login(
+      loginReqDto.email,
+      loginReqDto.password,
+    );
+
+    res
+      .cookie('refreshToken', refreshToken, { httpOnly: true })
+      .setHeader('Authorization', `Bearer ${accessToken}`);
+
+    return user;
   }
 }
